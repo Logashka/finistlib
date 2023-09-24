4736eb05f84efea4154d9acc104e1264a4f47130
Logashka <81705747+Logashka@users.noreply.github.com>
2023-09-24 17:13:45 +0300

    Update finist.ino

--- finist.ino
+++ finist.ino
@@ -3,6 +3,7 @@
 void setup() {
   // put your setup code here, to run once:
   LineSensor left_sensor(A0);
+  Encoder left_encoder(4,5,2);
   Serial.begin(9600);
   Serial.println(left_sensor.s_port);
 }
