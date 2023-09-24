67863d2d3452e824de314e3172a6ddb859a19c2a
Logashka <81705747+Logashka@users.noreply.github.com>
2023-09-24 21:38:14 +0300

    Update finist.ino

--- finist.ino
+++ finist.ino
@@ -1,7 +1,12 @@
-#include "finistSensor.h"
+#include "finistBase.h"
+
+//тут дефайны
+
 Encoder* left_encoder;
 Encoder* right_encoder;
-
+LineSensor right_line(A0);
+LineSensor left_line(A1);
+Base robot(25,25);
 
 void setup() {
   // put your setup code here, to run once:
@@ -9,7 +14,9 @@ void setup() {
   right_encoder = new Encoder (6, 7, 3);
   left_encoder->setup([]{left_encoder->EncoderEvent();});
   right_encoder->setup([]{right_encoder->EncoderEvent();});
-
+  robot.setup_lineSensors(left_line, right_line);
+  robot.setup_encoders(left_encoder, right_encoder);
+  
 }
 
 void loop() {
