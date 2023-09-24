afd8efce5e5547aad6c8ab31603d4f28c0c4c598
Logashka <81705747+Logashka@users.noreply.github.com>
2023-09-24 22:09:09 +0300

    Update finist.ino

--- finist.ino
+++ finist.ino
@@ -6,17 +6,20 @@ Encoder* left_encoder;
 Encoder* right_encoder;
 LineSensor right_line(A0);
 LineSensor left_line(A1);
+Motor left_motor(8, 9, 10);
+Motor right_motor(11, 12, 13);
 Base robot(25,25);
 
 void setup() {
-  // put your setup code here, to run once:
   left_encoder = new Encoder (4,5,2);
   right_encoder = new Encoder (6, 7, 3);
   left_encoder->setup([]{left_encoder->EncoderEvent();});
   right_encoder->setup([]{right_encoder->EncoderEvent();});
+
+
   robot.setup_lineSensors(left_line, right_line);
   robot.setup_encoders(left_encoder, right_encoder);
-  
+  robot.setup_motors(left_motor, right_motor);
 }
 
 void loop() {
