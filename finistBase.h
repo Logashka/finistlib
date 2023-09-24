6b65f198931f5ff8dc206857dac21c3b1764c588
Logashka <81705747+Logashka@users.noreply.github.com>
2023-09-24 22:10:05 +0300

    Update finistBase.h

--- finistBase.h
+++ finistBase.h
@@ -1,25 +1,38 @@
 #include "finistSensor.h"
+#include "finistMotor.h"
 
-class Base
-{
+class Base {
+private:
+    LineSensor *left_line;
+    LineSensor *right_line;
+    Encoder *right_encoder;
+    Encoder *left_encoder;
+    Motor *right_motor;
+    Motor *left_motor;
 public:
     float d, b;
-    LineSensor* left_line;
-    LineSensor* right_line;
-    Encoder* right_encoder;
-    Encoder* left_encoder;
-    Base(float wheel_diameter, float distance_between_wheels){
+
+
+    Base(float wheel_diameter, float distance_between_wheels) {
         d = wheel_diameter;
         b = distance_between_wheels;
     }
 
-    void setup_lineSensors(LineSensor &left, LineSensor &right){
+
+    void setup_lineSensors(LineSensor &left, LineSensor &right) {
         this->right_line = &right;
         this->left_line = &left;
     }
 
-    void setup_encoders(Encoder* left, Encoder* right){
+
+    void setup_encoders(Encoder *left, Encoder *right) {
         this->right_encoder = right;
         this->left_encoder = left;
+    }
+
+
+    void setup_motors(Motor &left, Motor &right){
+        this->right_motor = &right;
+        this->left_motor = &left;
     }
 };
