796a97bc143bdec06f2a0164abc59e78df9a5695
Logashka <81705747+Logashka@users.noreply.github.com>
2023-09-24 17:13:15 +0300

    Update finistBase.h

---
+++ finistBase.h
@@ -0,0 +1,12 @@
+#include "finistSensor.h"
+
+class Base
+{
+public:
+    float d, b;
+    Base(float wheel_diameter, float distance_between_wheels){
+        d = wheel_diameter;
+        b = distance_between_wheels;
+    }
+    void setup_encoders()
+};
