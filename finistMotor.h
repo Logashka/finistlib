44f37edeb10fc12397bf2b7cb48226df9ea7a6f7
Logashka <81705747+Logashka@users.noreply.github.com>
2023-09-26 14:46:25 +0300

    Update finistMotor.h

--- finistMotor.h
+++ finistMotor.h
@@ -4,29 +4,51 @@
  * dp2 - номер порта, управляющего направлением (int)
  * sp - номер порта, управляющего скоростью (int)
  *
- * если используется не драйвер, а амперка шилд, то dp2 = -1
- * если на пине скорости на драйвере стоит перемычка, sp не указывать
- *
- * !!! sp - именованный аргумент
- *
- * пример:
- * Motor rm(4,5,sp=6);
+ * пример для l298n:
+ * Motor rm(4,5,6);
+ * 
+ * пример для MotorShield
+ * Motor rm(4,5);
+ * 
+ * сейчас всё прописано для амперка мотор шилда, чуть позднее напишу для l298n
  */
 class Motor{
 private:
-    int dir_port1, dir_port2, speed_port;
+    int dir_port1, speed_port;
 public:
-    Motor(int dp1, int dp2, int sp = -1){
+    Motor(int dp1, int sp){
         dir_port1 = dp1;
-        dir_port2 = dp2;
         speed_port = sp;
         pinMode(dir_port1, OUTPUT);
-        pinMode(dir_port2, OUTPUT);
         pinMode(speed_port, OUTPUT);
     }
 
-    void move(){
-        //TODO: all...
+
+    //запускает мотор со скоростью в диапазоне +-0...+-256
+    void start(int speed){
+        if(speed >= 0){
+            digitalWrite(dir_port1, HIGH);
+        }
+        else{
+            digitalWrite(dir_port1, LOW);
+        }
+        digitalWrite(speed_port, abs(speed));
     }
 
+
+    //запускает моторы в процентах скорости
+    void start_percent(int speed){
+        this->start(speed/100*256);
+    }
+
+    //остановка. true - жёсткая, false - плавная. на амперка шилде работает через ....
+    void stop(bool type){
+        if(type){
+            digitalWrite(dir_port1, HIGH);
+        }
+        else{
+            digitalWrite(dir_port1, LOW);
+        }
+        digitalWrite(speed_port, 0);
+    }
 };
