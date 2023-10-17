8fa7947b7b570a062d0849904652319daf873b75
Logashka <81705747+Logashka@users.noreply.github.com>
2023-10-17 16:14:50 +0300

    Update finistBase.h

--- finistBase.h
+++ finistBase.h
@@ -24,6 +24,7 @@ public:
     Encoder *left_encoder;
     Motor *right_motor;
     Motor *left_motor;
+    Sharp *dist_sensor;
 
     Base(float wheel_diameter, float distance_between_wheels) {
         d = wheel_diameter;
@@ -55,19 +56,32 @@ public:
      * left - объект(Motor) левого двигателя
      * right - объект(Motor) правого двигателя
      */
-    void setup_motors(Motor &left, Motor &right){
+    void setup_motors(Motor &left, Motor &right) {
         this->right_motor = &right;
         this->left_motor = &left;
     }
 
+
+    void setup_sharp(Sharp &sens){
+        this->dist_sensor = &sens;
+    }
     //запускает два мотора с одинаковой скоростью в процентах
-    void w2_start(int speed){
+    void w2_start(int speed) {
         left_motor->start_percent(speed);
         right_motor->start_percent(speed);
     }
 
-    void w2_startSync(int speed_left, int speed_right){
+
+    //запускает два мотора с разными скоростями в процентах
+    void w2_startSync(int speed_left, int speed_right) {
         left_motor->start_percent(speed_left);
         right_motor->start_percent(speed_right);
+    }
+
+
+    //остановка. true-жёсткая, false-накатом
+    void w2_stop(bool type) {
+        left_motor->stop(type);
+        right_motor->stop(type);
     }
 };
