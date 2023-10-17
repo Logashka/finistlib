77bcfef5bda0a747e41a12c56c73f0bbbd5f6dba
Logashka <81705747+Logashka@users.noreply.github.com>
2023-10-17 16:14:18 +0300

    Update finistMotor.h

--- finistMotor.h
+++ finistMotor.h
@@ -1,3 +1,4 @@
+#include <ServoSmooth.h>
 /*объект DC мотора.
  * входные параметры:
  * dp1 - номер порта, управляющего направлением (int)
@@ -15,34 +16,34 @@
  *
  * ! обязательно прописать name.setup() в сетап
  */
-class Motor{
+class Motor {
 private:
     int dir_port1, speed_port;
 public:
     int k = 1;
-    Motor(int dp1, int sp){
+
+    Motor(int dp1, int sp) {
         dir_port1 = dp1;
         speed_port = sp;
     }
 
 
     //пинмоды, инвертирование true/false
-    void setup(bool invert = false){
+    void setup(bool invert = false) {
         pinMode(dir_port1, OUTPUT);
         pinMode(speed_port, OUTPUT);
-        if(invert){
+        if (invert) {
             k = -1;
         }
     }
 
 
     //запускает мотор со скоростью в диапазоне +-0...+-256
-    void start(float speed){
+    void start(float speed) {
         speed *= k;
-        if(speed >= 0){
+        if (speed >= 0) {
             digitalWrite(dir_port1, HIGH);
-        }
-        else{
+        } else {
             digitalWrite(dir_port1, LOW);
         }
         analogWrite(speed_port, int(fabs(speed)));
@@ -50,17 +51,16 @@ public:
 
 
     //запускает моторы в процентах скорости
-    void start_percent(float speed){
+    void start_percent(float speed) {
         float v = map(speed, 0, 100, 0, 255);
         this->start(v);
     }
 
     //остановка. true - жёсткая, false - плавная. на амперка шилде работает через ....
-    void stop(bool type){
-        if(type){
+    void stop(bool type) {
+        if (type) {
             digitalWrite(dir_port1, HIGH);
-        }
-        else{
+        } else {
             digitalWrite(dir_port1, LOW);
         }
         digitalWrite(speed_port, 0);
